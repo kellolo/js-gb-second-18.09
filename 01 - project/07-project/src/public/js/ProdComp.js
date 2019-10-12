f807359296aa329ee3fdf06b059fed6497098eaf
kellolo <87kell@gmail.com>
2019-10-12 15:28:20 +0200

    8

---
+++ 01 - project/07-project/src/public/js/ProdComp.js
@@ -0,0 +1,49 @@
+let product = {
+    props: ['product', 'img'],
+    template: `<div class="product-item" >
+                <img :src="img" alt="Some img">
+                <div class="desc">
+                    <h3>{{ product.product_name }}</h3>
+                    <p>{{ product.price }} $</p>
+                    <button class="buy-btn" @click="$root.$refs.cart.addProduct (product)">Купить</button>
+                </div>
+            </div>`
+}
+
+let products = {
+    data () {
+        return {
+            products: [],
+            imgCatalog: 'https://placehold.it/200x150',
+            catalogUrl: '/catalogData.json',
+            filtered: []
+        }
+    },
+    mounted(){
+        this.$parent.getJson(`/api/products`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+    },
+    methods: {
+        filter(value){
+            let regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    template: `<div class="products">
+                    <product 
+                    v-for="product of products" 
+                    :key="product.id_product"
+                    :img="imgCatalog"
+                    :product="product"></product>
+                </div>`,
+    components: {
+        product
+    }
+}
+
+export default products
